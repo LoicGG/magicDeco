9fa5d7a12ccc73268ce068200cdfcf85804a2299
LOIC GREBERT <92549640+LoicGG@users.noreply.github.com>
2023-11-27 11:25:42 +0100

    on scroll close nav bar

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 /************  NAVIGATION BAR ****************/
+/************  NAVIGATION BAR ****************/
+
+const links = document.querySelectorAll("nav li");
+
+iconX.addEventListener("click",() => {
+  nav.classList.toggle("active");
+});
+
+addEventListener("scroll",() => {
+    nav.classList.remove("active");
+});
+
+
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        nav.classList.remove("active");
+    });
+});
+
+/***************END OF NAV BAR ****************/
